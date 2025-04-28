c4a3bbbc57d3c5674dd748cab2021f883018e4e1
Mohanragul-01 <mohanragul75@gmail.com>
2025-04-28 14:13:21 +0530

    first-commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import random
+
+app = Flask(__name__)
+
+choices = ['rock', 'paper', 'scissors']
+
+def get_result(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'paper' and computer_choice == 'rock') or \
+         (user_choice == 'scissors' and computer_choice == 'paper'):
+        return "You win!"
+    else:
+        return "You lose!"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_choice = None
+    computer_choice = None
+    result = None
+
+    if request.method == 'POST':
+        user_choice = request.form['choice']
+        computer_choice = random.choice(choices)
+        result = get_result(user_choice, computer_choice)
+
+    return render_template('index.html', user_choice=user_choice, computer_choice=computer_choice, result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
